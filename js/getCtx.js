737f41a1c8245e6b7db6c62dac3ff5b4d29572d5
ChenWenHao <10602429@qq.com>
2017-04-05 10:31:58 +0800

    ss

--- js/getCtx.js
+++
@@ -1,9 +0,0 @@
-function getCtx(containerSelector, width, height) {
-	var container = document.querySelector(containerSelector);
-	var cvs = document.createElement('canvas');
-	cvs.width = width;
-	cvs.height = height;
-	cvs.style = 'border: 1px solid red';
-	container.appendChild(cvs);
-	return cvs.getContext('2d');
-}
--- js/random.js
+++
@@ -1,4 +0,0 @@
-// 在指定范围内生成随机数
-function getRandom(min, max) {
-	return Math.random() * (max - min) + min;
-}
